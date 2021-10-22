1839559a1e943393e54d3bdd303f99817af6aa35
Matt <matt@stallings.us>
2021-10-21 20:42:05 -0500

    Initial commit

---
+++ src/main/java/us/stallings/ssch3/SsCh3Application.java
@@ -0,0 +1,13 @@
+package us.stallings.ssch3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsCh3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsCh3Application.class, args);
+    }
+
+}
---
+++ src/test/java/us/stallings/ssch3/SsCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package us.stallings.ssch3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsCh3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
