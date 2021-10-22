a6b80bfd957f34973fd64b2c3334aca552941a19
Matt <matt@stallings.us>
2021-10-21 20:50:31 -0500

    Configuration of security using JDBCUserDetailsManager.

---
+++ src/main/java/us/stallings/ssch3/controller/HelloController.java
@@ -0,0 +1,12 @@
+package us.stallings.ssch3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+}
